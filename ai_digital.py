5215a6773b36f9bd8b9040f9e419c77f0392cb72
Skylerrr07 <135133509+Skylerrr07@users.noreply.github.com>
2023-06-29 14:41:08 +0800

    Update ai_digital.py

--- ai_digital.py
+++ ai_digital.py
@@ -11,6 +11,7 @@ def index():
 def generate_text():
     # 获取文本框1中的关键字
     keyword = request.form['keyword']
+    print(keyword)
     # 调用AI生成文本的函数，生成文章
     generated_text = generate_text_function(keyword)
     # 将生成的文章返回给前端
@@ -19,12 +20,14 @@ def generate_text():
 @app.route('/generate_video', methods=['POST'])
 def generate_video():
     # 获取文本框2中的文章内容
-    article = request.form['article']
+    article = request.form['generatedText']
     # 获取其他选项的值，如数字人播报、视频比例、背景音乐和朗读音色
-    narrator = request.form['narrator']
-    video_ratio = request.form['video_ratio']
-    background_music = request.form['background_music']
-    voice_tone = request.form['voice_tone']
+    narrator = request.form['avatar']
+    video_ratio = request.form['videoRatio']
+    background_music = request.form['music']
+    voice_tone = request.form['voice']
+    
+    print(narrator, video_ratio, background_music, voice_tone)
     # 调用AI生成视频的函数，生成视频并返回生成进度
     progress = generate_video_function(article, narrator, video_ratio, background_music, voice_tone)
     # 将生成进度返回给前端
